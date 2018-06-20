3ce51c7410a00d3020310f6f5d077d636e5fd8ba
Michał Szydłowski <michal.szydlowski@droidsonroids.pl>
2018-06-20 18:13:51 +0200

    Added Yaml sanitiser

--- dispatcher/src/main/kotlin/pl/droidsonroids/testing/mockwebserver/YamlResourcesParser.kt
+++ dispatcher/src/main/kotlin/pl/droidsonroids/testing/mockwebserver/YamlResourcesParser.kt
@@ -1,14 +1,23 @@
 package pl.droidsonroids.testing.mockwebserver
 
+import org.apache.commons.text.translate.AggregateTranslator
+import org.apache.commons.text.translate.EntityArrays
+import org.apache.commons.text.translate.JavaUnicodeEscaper
+import org.apache.commons.text.translate.LookupTranslator
 import org.yaml.snakeyaml.Yaml
-
+  
 internal class YamlResourcesParser : ResourcesParser {
-    private val parser = Yaml()
-
-    override fun parseFrom(fileName: String): Fixture {
-        val path = "fixtures/$fileName.yaml"
-        val content = path.getResourceAsString()
-        val result = parser.loadAs(content, Fixture::class.java)
+    private val escaper = AggregateTranslator(
+        LookupTranslator(EntityArrays.JAVA_CTRL_CHARS_ESCAPE),
+        JavaUnicodeEscaper.outsideOf(32, 0x7f)
+    )
+      private val parser = Yaml()
+  
+      override fun parseFrom(fileName: String): Fixture {
+          val path = "fixtures/$fileName.yaml"
+          val content = path.getResourceAsString()
+        val escapedContent = escaper.translate(content)
+        val result = parser.loadAs(escapedContent, Fixture::class.java)
 
         if (!result.hasJsonBody()) {
             val bodyPath = "fixtures/${result.body}"
